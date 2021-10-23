1b34e8510841387fc41d95a03b8fc7cda4ce5ce9
Egogorka <bike034@mail.ru>
2021-10-23 08:45:28 +0300

    something x2

--- main.cpp
+++ main.cpp
@@ -1,31 +1,36 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
+
+#include "src/utility/Vector.h"
 
 int main()
 {
-    // create the window
-    sf::RenderWindow window(sf::VideoMode(800, 600), "My window");
+//    // create the window
+//    std::cout << "test";
+//    sf::RenderWindow window(sf::VideoMode(800, 600), "My window");
+//
+//    // run the program as long as the window is open
+//    while (window.isOpen())
+//    {
+//        // check all the window's events that were triggered since the last iteration of the loop
+//        sf::Event event;
+//        while (window.pollEvent(event))
+//        {
+//            // "close requested" event: we close the window
+//            if (event.type == sf::Event::Closed)
+//                window.close();
+//        }
+//
+//        // clear the window with black color
+//        window.clear(sf::Color::Green);
+//
+//        // draw everything here...
+//        // window.draw(...);
+//
+//        // end the current frame
+//        window.display();
+//    }
 
-    // run the program as long as the window is open
-    while (window.isOpen())
-    {
-        // check all the window's events that were triggered since the last iteration of the loop
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            // "close requested" event: we close the window
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
-
-        // clear the window with black color
-        window.clear(sf::Color::Green);
-
-        // draw everything here...
-        // window.draw(...);
-
-        // end the current frame
-        window.display();
-    }
 
     return 0;
 }
